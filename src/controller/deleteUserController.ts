f0de3de79d1152350f441da96dc615f580dd4deb
Aristoteles <arystotelys@gmail.com>
2023-04-24 14:02:36 -0300

    clear tabela de users

--- src/controller/deleteUserController.ts
+++ src/controller/deleteUserController.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import { deleteUserServices } from "../services/getAdmServices copy";
+import { deleteUserServices } from "../services/deleteUserServices";
 
 
 class deleteUserController{
